5c4c22e51f1f60b3c3afeebdb493350dfcc804f2
MSI\amoun <amouniverso@yandex.ru>
2021-11-21 14:18:37 +0300

    fonts

--- Assets/@Core/Scripts/ChickenBehavior.cs
+++ Assets/@Core/Scripts/ChickenBehavior.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChickenRotation : MonoBehaviour
+public class ChickenBehavior : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed;
+    [SerializeField] private float moveSpeed;
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(new Vector3(rotationSpeed * Time.deltaTime, rotationSpeed * Time.deltaTime, rotationSpeed * Time.deltaTime));
+        transform.Translate(moveSpeed * (Vector3.forward - Vector3.right) * Time.deltaTime, Space.World);
     }
 }
